f3e784e6cf909d44c5ef4cb2f5f285cf1f00ca3d
Kaouru Tanamachi <lg489119@gmail.com>
2025-04-13 16:54:13 -0600

    Update InicioInterfaz.py
    Se agregó menú de opciones a la interfaz de inicio

--- InicioInterfaz.py
+++ InicioInterfaz.py
@@ -3,5 +3,15 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Inicio")
 ventana.geometry("800x600+100+50")
+# BARRA DE MENU
+barra_menu = tk.Menu(ventana)
+ventana.config(menu=barra_menu) 
+
+#MENU DE ARCHIVO
+menu_opciones = tk.Menu(barra_menu, tearoff=0, bg="lightblue", fg="black")
+menu_opciones.add_command(label="Ajustes")
+menu_opciones.add_separator()
+menu_opciones.add_command(label="Salir", command=ventana.quit)
+barra_menu.add_cascade(label="Opciones", menu=menu_opciones)
 #MANTIENE LA VENTANA ABIERTA
 ventana.mainloop()
