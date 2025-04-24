ee6774bc66fcc3774b4283df9c450742fc663334
Kaouru Tanamachi <lg489119@gmail.com>
2025-04-23 20:14:00 -0600

    Comentarios de funcionamiento
    Se agrego explicacion de que hace cada parte del codigo por el momento a main e inciointerfaz

--- InicioInterfaz.py
+++ InicioInterfaz.py
@@ -1,35 +1,45 @@
-import tkinter as tk
-import sys
+#INTERFAZ DE INICIO (no confundir con login)
+#----Bibliotecas----
+import tkinter as tk #Tkinter que es la libreria para la interfaz, que se importa como tk =Tkinter
+import sys #Importamos Sys 
 
-class MainView(tk.Toplevel):
-    def __init__(self, master, controller, username='', is_admin=False):
+#----Clase para Interfaz de inicio----
+class MainView(tk.Toplevel):#Toplevel es para que la ventana pase a ser la principal
+    """___init___ es el constructor de una clase"""
+    
+    def __init__(self, master, controller, username='', is_admin=False): #Recibe master,controller, username y is_admin de funciones.py
+        """Self se usa para hacer un atributo de otra funcion o clase como propio."""
         super().__init__(master)
-        self.controller = controller
-        self.username = username #Recibe Nombre de Usuario
-        self.is_admin = is_admin #Recibe si es administrador
+        self.controller = controller #Recibe controller y lo hace un atributo propio 
+        self.username = username #Recibe username(Nombre de Usuario) y lo hace un atributo propio 
+        self.is_admin = is_admin #Recibe is_admin y lo hace un atributo propio, en este caso (1 o 0) 
 
-        titulo = f"Inicio - {self.username}"
-        if self.is_admin:#validador de usuario tipo administrador
-            titulo += " (Administrador)" #Si es administrador agregar "administrador"
-        self.title(titulo)#titulo
+        titulo = f"Inicio - {self.username}" #Crea un atributo titulo que en este caso llevara la palabra Inicio + lo que haya en username
+        if self.is_admin:#validador de usuario tipo administrador, si is_admin es true entra al if.
+            titulo += " (Administrador)" #Si es administrador agregar "administrador" al titulo.
+        self.title(titulo)#Inserta titulo en la parte de arriba en la interfaz
 
-        self.geometry("800x600+100+50")#Tamaño de ventana
-        self._make_menu()#menu
+        self.geometry("800x600+100+50")#Tamaño de ventana(<ancho>x<alto>±<posición_x>±<posición_y>)
+        self._make_menu()#ejecuta menu
         
+    
+    
+    #----Funcion de Menu en interfaz ----
     def _make_menu(self):
-        self.barra_menu = tk.Menu(self)#Agrega la barra de menu a menu
-        self.config(menu=self.barra_menu) 
+        self.barra_menu = tk.Menu(self) #Agrega el elemento menu de tkinter a barra_menu
+        self.config(menu=self.barra_menu) #Agrega la barra de menu a menu y menu a config
         #Menu
-        menu_opciones = tk.Menu(self.barra_menu, tearoff=0, bg="lightblue", fg="black")
-        menu_opciones.add_command(label="Ajustes")
+        menu_opciones = tk.Menu(self.barra_menu, tearoff=0, bg="lightblue", fg="black") #Se aagrega un submenu que se desplega en barra_menu y su configuracion visual.
+        menu_opciones.add_command(label="Ajustes") #añade un boton al submenu con la opcion de ajustes, por el momento no hace nada.
         #Opciones solo para administrador
-        if self.is_admin:
-            menu_opciones.add_separator()
-            menu_opciones.add_command(
-                label="Agregar nuevo Usuario", 
-                command=self.controller.show_register  
-            )
+        if self.is_admin: #validador si is_admin es true mostrara lo siguiente en el menu
+            menu_opciones.add_separator() #Añade un separador en el submenu
+            menu_opciones.add_command(label="Nuevo Usuario", command=self.controller.show_register) 
+            """añade un boton al submenu con la opcion para añadir nuevo usuario y manda a llamar controller para abrir la interfaz de show_register"""
         menu_opciones.add_separator()
         menu_opciones.add_command(label="Cerrar sesión", command=self.controller.logout)
+        """añade un boton al submenu con la opcion para añadir nuevo usuario y manda a llamar controller para abrir la interfaz de login"""
         menu_opciones.add_command(label="Salir", command=sys.exit)
+        """añade un boton al submenu con la opcion para Salir y hace uso de la biblioteca Sys,
+        esto es para que se cierre completamente el programa y que cualquier subproceso que haya quedado, se termine completamente."""
         self.barra_menu.add_cascade(label="Opciones", menu=menu_opciones)#Agrega el boton "Opciones" a la barra de menu
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #INICIALIZADOR
-from funciones import AppController
-if __name__ == "__main__":
-    app = AppController()
-    app.login_view.mainloop() 
+#----Bibliotecas----
+from funciones import AppController 
+"""se importa AppControler de funciones.py"""
+
+if __name__ == "__main__":#se crea un main algo como en C++
+    app = AppController() #Se asigna AppController a app
+    app.login_view.mainloop() #de app se obtiene y ejecuta login_view
+"""para ejecutar una funcion en python basta con poner el nombre de la funcion y (), ejemplo: app() con esto se ejecutaria app que es AppController,
+pero en este caso ocupamos solo el login de AppController"""
